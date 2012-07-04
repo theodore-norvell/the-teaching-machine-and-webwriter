0ab34a66c86e0ca49b652121ca3e63002c9eebab
siddharth.shah.mail@gmail.com <siddharth.shah.mail@gmail.com@d93f6c20-76b8-25b4-6ab2-a62924e335af>
2012-07-04 13:50:08 +0000

    Updated trial

--- trunk/trunk/higraphTests/trial_final.cpp
+++ trunk/trunk/higraphTests/trial_final.cpp
@@ -13,37 +13,58 @@ int main()
      setDefaultNodeValueShow(true, CENTER);
      setDefaultNodeValueColor(MARKER_RED);
      setDefaultNodeShape(RECTANGLE);
-     ScriptManager::relay("HigraphManager", "setDefaultNodeFillColor", BLUE);
+	 //ScriptManager::relay("HigraphManager", "setDefaultZoneColor", BLUE);
+     ScriptManager::relay("HigraphManager", "setDefaultNodeFillColor", WHITE);
      ScriptManager::relay("HigraphManager", "setDefaultNodeValueShow", true, CENTER);
-     ScriptManager::relay("HigraphManager", "setDefaultNodeValueColor", WHITE);
+     ScriptManager::relay("HigraphManager", "setDefaultNodeValueColor", BLACK);
      ScriptManager::relay("HigraphManager", "setDefaultNodeNameShow", true, EAST);
-     ScriptManager::relay("HigraphManager", "setDefaultNodeNameColor", BLUE);
+     ScriptManager::relay("HigraphManager", "setDefaultNodeNameColor", MAGENTA);
+	 ScriptManager::relay("HigraphManager","setDefaultNodeSize", 70, 70);
        
      /*#/TS*/
-     const int rows = 2;
-     const int cols = 3;
-     int matrix1[rows][cols];/*#TS*/int **mat = new int*[rows];
-     for(int i=0;i<rows;i++)
-        mat[i]=matrix1[i];
-     makeMatrix(mat,rows,cols,true,"matrix1");/*#/TS*/
-     int matrix2[rows*cols];
-     /*#TS*/makeArray(matrix2,(rows*cols),true,"matrix2");/*#/TS*/
-     for(int i = 0; i < rows; i++)
-     {
-        for(int j = 0; j < cols; j++)
-            {
-                matrix1[i][j]=i+j;
-                
-            }
-     }
+     const int rows = 3;
+     const int cols = 4;
+	 int val = 10;/*#TS*/ //setupval(val);
+	 /*#/TS*/
      
-     for(int i = 0; i < rows; i++)
+     int matrix[rows*cols];
+     /*#TS*/makeArray(matrix,(rows*cols),true,"Actual array");/*#/TS*/
+	 int i,j;/*#TS*//*nodes for i & j*///setup_i_j(i,j);
+	 ScriptManager::relay("HigraphManager","makeNode", i);
+	 ScriptManager::relay("HigraphManager","dislocate", i, 360, 120);
+     ScriptManager::relay("HigraphManager","setNodeValueShow", i, true);
+	 ScriptManager::relay("HigraphManager","setNodeNameLabel", i, "i");
+	 ScriptManager::relay("HigraphManager","setNodeNameNudge",i,-10,0);
+     ScriptManager::relay("HigraphManager","setNodeShape", i, ELLIPSE);
+	 ScriptManager::relay("HigraphManager","setNodeColor", i, BLACK);
+     ScriptManager::relay("HigraphManager","setNodeFillColor", i, YELLOW);
+     ScriptManager::relay("HigraphManager","setNodeNamePosition", i, WEST);
+	 
+	 ScriptManager::relay("HigraphManager","makeNode", j);
+	 ScriptManager::relay("HigraphManager","dislocate", j, 500, 120);
+     ScriptManager::relay("HigraphManager","setNodeValueShow", j, true);
+	 ScriptManager::relay("HigraphManager","setNodeNameLabel", j, "j");
+	 ScriptManager::relay("HigraphManager","setNodeNameNudge",j,-10,0);
+	 ScriptManager::relay("HigraphManager","setNodeColor", j, BLACK);
+     ScriptManager::relay("HigraphManager","setNodeFillColor", j, YELLOW);
+     ScriptManager::relay("HigraphManager", "setNodeShape", j, ELLIPSE);
+     ScriptManager::relay("HigraphManager","setNodeNamePosition", j, WEST);/*#/TS*/
+	 
+	 /*#TS*/int matrix1[rows][cols];int **mat = new int*[rows];
+     for(int k=0;k<rows;k++)
+        mat[k]=matrix1[k];
+     makeMatrix(mat,rows,cols,true,"Theoretical matrix represenation");/*#/TS*/
+	 
+     for(i = 0; i < rows; i++)
      {
-        for(int j = 0; j < cols; j++)
-            {
-                matrix2[(i*cols)+j]=i+j;
-                
-            }
+        for(j = 0; j < cols; j++)
+            {	/*#TS*/ScriptManager::relay("HigraphManager","setNodeFillColor", matrix1[i][j], YELLOW);
+					ScriptManager::relay("HigraphManager","setNodeFillColor", matrix[(i*cols)+j], YELLOW);/*#/TS*/
+				/*#TS*/	matrix1[i][j]=val;/*#/TS*/
+                matrix[(i*cols)+j]=val++;
+				/*#TS*/ScriptManager::relay("HigraphManager","setNodeFillColor", matrix1[i][j], WHITE);
+					ScriptManager::relay("HigraphManager","setNodeFillColor", matrix[(i*cols)+j], WHITE);/*#/TS*/
+			}
      }
      
      /*#TS*/
