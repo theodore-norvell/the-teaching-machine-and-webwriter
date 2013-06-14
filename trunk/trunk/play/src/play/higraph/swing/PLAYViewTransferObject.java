a3dc85ff6468f045c8bf9541f82b9ffc8969b655
ravneetsandhu.87@gmail.com <ravneetsandhu.87@gmail.com@d93f6c20-76b8-25b4-6ab2-a62924e335af>
2013-06-14 18:01:20 +0000

    Fixed type of selection list

--- trunk/trunk/play/src/play/higraph/swing/PLAYViewTransferObject.java
+++ trunk/trunk/play/src/play/higraph/swing/PLAYViewTransferObject.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.util.List;
 
 import play.higraph.model.PLAYTag;
+import play.higraph.view.PLAYNodeView;
 
 /**
  * @author Kai Zhu
@@ -19,63 +20,63 @@ import play.higraph.model.PLAYTag;
  */
 public class PLAYViewTransferObject implements Transferable {
 
-    private PLAYTag tag;
+	private PLAYTag tag;
 
-    private List<?> viewList;
+	private List<PLAYNodeView> viewList;
 
-    public static final DataFlavor TAG_DATAFLAVOR = new DataFlavor(
-	    PLAYTag.class, "PLAYTag.DataFlavor");
+	public static final DataFlavor TAG_DATAFLAVOR = new DataFlavor(
+			PLAYTag.class, "PLAYTag.DataFlavor");
 
-    public static final DataFlavor VIEWLIST_DATAFLAVOR = new DataFlavor(
-	    List.class, "ViewList.DataFlavor");
+	public static final DataFlavor VIEWLIST_DATAFLAVOR = new DataFlavor(
+			List.class, "ViewList.DataFlavor");
 
-    public PLAYViewTransferObject(Object object) {
-	if (object instanceof PLAYTag) {
-	    this.tag = (PLAYTag) object;
-	} else if (object instanceof List) {
-	    this.viewList = (List<?>) object;
+	public PLAYViewTransferObject(PLAYTag tag) {
+		this.tag = tag;
 	}
-    }
-
-    /**
-     * @see java.awt.datatransfer.Transferable#getTransferDataFlavors()
-     */
-    @Override
-    public DataFlavor[] getTransferDataFlavors() {
-	return new DataFlavor[] { DataFlavor.stringFlavor,
-		PLAYViewTransferObject.TAG_DATAFLAVOR,
-		PLAYViewTransferObject.VIEWLIST_DATAFLAVOR };
-    }
-
-    /**
-     * @see java.awt.datatransfer.Transferable#isDataFlavorSupported(java.awt.datatransfer.DataFlavor)
-     */
-    @Override
-    public boolean isDataFlavorSupported(DataFlavor flavor) {
-	if (flavor.equals(DataFlavor.stringFlavor)) {
-	    return true;
-	} else if (flavor.equals(PLAYViewTransferObject.TAG_DATAFLAVOR)) {
-	    return true;
-	} else if (flavor.equals(PLAYViewTransferObject.VIEWLIST_DATAFLAVOR)) {
-	    return true;
+	
+	public PLAYViewTransferObject( List<PLAYNodeView> list) {
+		this.viewList =  list;
 	}
-	return false;
-    }
 
-    /**
-     * @see java.awt.datatransfer.Transferable#getTransferData(java.awt.datatransfer.DataFlavor)
-     */
-    @Override
-    public Object getTransferData(DataFlavor flavor)
-	    throws UnsupportedFlavorException, IOException {
-	if (flavor.equals(DataFlavor.stringFlavor)) {
-	    return flavor.toString();
-	} else if (flavor.equals(PLAYViewTransferObject.TAG_DATAFLAVOR)) {
-	    return this.tag;
-	} else if (flavor.equals(PLAYViewTransferObject.VIEWLIST_DATAFLAVOR)) {
-	    return this.viewList;
+	/**
+	 * @see java.awt.datatransfer.Transferable#getTransferDataFlavors()
+	 */
+	@Override
+	public DataFlavor[] getTransferDataFlavors() {
+		return new DataFlavor[] { DataFlavor.stringFlavor,
+				PLAYViewTransferObject.TAG_DATAFLAVOR,
+				PLAYViewTransferObject.VIEWLIST_DATAFLAVOR };
 	}
-	return null;
-    }
+
+	/**
+	 * @see java.awt.datatransfer.Transferable#isDataFlavorSupported(java.awt.datatransfer.DataFlavor)
+	 */
+	@Override
+	public boolean isDataFlavorSupported(DataFlavor flavor) {
+		if (flavor.equals(DataFlavor.stringFlavor)) {
+			return true;
+		} else if (flavor.equals(PLAYViewTransferObject.TAG_DATAFLAVOR)) {
+			return true;
+		} else if (flavor.equals(PLAYViewTransferObject.VIEWLIST_DATAFLAVOR)) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * @see java.awt.datatransfer.Transferable#getTransferData(java.awt.datatransfer.DataFlavor)
+	 */
+	@Override
+	public Object getTransferData(DataFlavor flavor)
+			throws UnsupportedFlavorException, IOException {
+		if (flavor.equals(DataFlavor.stringFlavor)) {
+			return flavor.toString();
+		} else if (flavor.equals(PLAYViewTransferObject.TAG_DATAFLAVOR)) {
+			return this.tag;
+		} else if (flavor.equals(PLAYViewTransferObject.VIEWLIST_DATAFLAVOR)) {
+			return this.viewList;
+		}
+		return null;
+	}
 
 }
