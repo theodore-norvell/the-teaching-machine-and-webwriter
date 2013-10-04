4d87a7bc882b70d36c380d661a1653865800e32b
ravneetsandhu.87 <ravneetsandhu.87@gmail.com>
2013-10-04 15:28:47 +0000

    removed the arrow

--- trunk/play/src/play/higraph/view/SEQNodeView.java
+++ trunk/play/src/play/higraph/view/SEQNodeView.java
@@ -27,95 +27,98 @@ import tm.backtrack.BTTimeManager;
  */
 public class SEQNodeView extends PLAYNodeView {
 
-    /**
-     * @param v
-     * @param node
-     * @param timeMan
-     */
-    protected SEQNodeView(
-	    HigraphView<PLAYPayload, PLAYEdgeLabel, PLAYHigraph, PLAYWholeGraph, PLAYSubgraph, PLAYNode, PLAYEdge> v,
-	    PLAYNode node, BTTimeManager timeMan) {
-	super(v, node, timeMan);
-	super.setNodeSize(200, 80);
-	super.setColor(Color.BLUE);
-	super.setFillColor(null);
-    }
-
-    /**
-     * @see higraph.view.NodeView#drawSelf(java.awt.Graphics2D)
-     */
-    @Override
-    protected void drawSelf(Graphics2D screen) {
-	double x = super.getNextX();
-	double y = super.getNextY();
-	int number = this.getNumChildren();
-
-	super.drawSelf(screen);
-
-	// draw a question mark
-	screen.drawString("\u21D2", (float) (x + 5), (float) (y + 10));
-
-	for (int i = 0; i < number - 1; i++) {
-	    x = super.getChild(i).getNextShapeExtent().getCenterX();
-	    y = super.getChild(i).getNextShapeExtent().getMaxY()
-		    + (super.getChild(i + 1).getNextShapeExtent().getMinY() - super
-			    .getChild(i).getNextShapeExtent().getMaxY()) / 2;
-	    screen.drawString("\u21D3", (float) x, (float) (y + 5));
+	/**
+	 * @param v
+	 * @param node
+	 * @param timeMan
+	 */
+	protected SEQNodeView(
+			HigraphView<PLAYPayload, PLAYEdgeLabel, PLAYHigraph, PLAYWholeGraph, PLAYSubgraph, PLAYNode, PLAYEdge> v,
+			PLAYNode node, BTTimeManager timeMan) {
+		super(v, node, timeMan);
+		super.setNodeSize(200, 200);
+		super.setColor(Color.BLUE);
+		super.setFillColor(null);
+		//System.out.println("sequence node view");
 	}
-    }
 
-    /**
-     * @see higraph.view.ComponentView#moveZone(higraph.view.ZoneView)
-     */
-    @Override
-    public void moveZone(
-	    ZoneView<PLAYPayload, PLAYEdgeLabel, PLAYHigraph, PLAYWholeGraph, PLAYSubgraph, PLAYNode, PLAYEdge> zone) {
-	PLAYNodeView nodeView = (PLAYNodeView) zone.getAssociatedComponent();
-	int number = super.getNumChildren();
-	double x = 0;
-	double y = 0;
-	double width = 0;
-	double height = 0;
-	if (number > 0) {
-	    // have a child or some children
-	    int index = super.indexOfZones(zone);
-	    if (index == 0) {
-		PLAYNodeView childNodeView = (PLAYNodeView) nodeView
-			.getChild(0);
-		x = childNodeView.getNextShapeExtent().getMinX();
-		y = nodeView.getNextShapeExtent().getMinY();
-		width = childNodeView.getNextShapeExtent().getWidth();
-		height = childNodeView.getNextShapeExtent().getMinY() - y;
-	    } else if ((index != 0) && (index < number)) {
-		PLAYNodeView leftChildNodeView = (PLAYNodeView) nodeView
-			.getChild(index - 1);
-		PLAYNodeView rightChildNodeView = (PLAYNodeView) nodeView
-			.getChild(index);
-		x = leftChildNodeView.getNextShapeExtent().getMinX();
-		y = leftChildNodeView.getNextShapeExtent().getMaxY();
-		width = rightChildNodeView.getNextShapeExtent().getWidth();
-		height = rightChildNodeView.getNextShapeExtent().getMinY() - y;
-	    } else if (index == number) {
-		PLAYNodeView childNodeView = (PLAYNodeView) nodeView
-			.getChild(number - 1);
-		x = childNodeView.getNextShapeExtent().getMinX();
-		y = childNodeView.getNextShapeExtent().getMaxY();
-		width = childNodeView.getNextShapeExtent().getWidth();
-		height = nodeView.getNextShapeExtent().getMaxY() - y;
-	    }
+	/**
+	 * @see higraph.view.NodeView#drawSelf(java.awt.Graphics2D)
+	 */
+	@Override
+	protected void drawSelf(Graphics2D screen) {
+		//System.out.println("drawself seq node");
+		double x = super.getNextX();
+		double y = super.getNextY();
+		int number = this.getNumChildren();
+		//System.out.println(number);
 
-	} else {
-	    // no children
-	    x = nodeView.getNextShapeExtent().getMinX()
-		    + nodeView.getNextShapeExtent().getWidth() / 10;
-	    y = nodeView.getNextShapeExtent().getMinY()
-		    + nodeView.getNextShapeExtent().getHeight() / 10;
-	    width = nodeView.getNextShapeExtent().getWidth() * 4 / 5;
-	    height = nodeView.getNextShapeExtent().getHeight() * 4 / 5;
+		super.drawSelf(screen);
+
+		// draw an arrow
+		//screen.drawString("\u21D2", (float) (x + 2), (float) (y + 10));
+
+		for (int i = 0; i < number - 1; i++) {
+			x = super.getChild(i).getNextShapeExtent().getCenterX();
+			y = super.getChild(i).getNextShapeExtent().getMaxY()
+					+ (super.getChild(i + 1).getNextShapeExtent().getMinY() - super
+							.getChild(i).getNextShapeExtent().getMaxY()) / 2;
+			screen.drawString("\u21D3", (float) x, (float) (y + 5));
+		}
 	}
-	zone.setNextShape(new Rectangle2D.Double(x, y, width, height));
-	zone.placeNext(x, y);
-	super.moveZone(zone);
-    }
+
+	/**
+	 * @see higraph.view.ComponentView#moveZone(higraph.view.ZoneView)
+	 */
+	@Override
+	public void moveZone(
+			ZoneView<PLAYPayload, PLAYEdgeLabel, PLAYHigraph, PLAYWholeGraph, PLAYSubgraph, PLAYNode, PLAYEdge> zone) {
+		PLAYNodeView nodeView = (PLAYNodeView) zone.getAssociatedComponent();
+		int number = super.getNumChildren();
+		double x = 0;
+		double y = 0;
+		double width = 0;
+		double height = 0;
+		if (number > 0) {
+			// have a child or some children
+			int index = super.indexOfZones(zone);
+			if (index == 0) {
+				PLAYNodeView childNodeView = (PLAYNodeView) nodeView
+						.getChild(0);
+				x = childNodeView.getNextShapeExtent().getMinX();
+				y = nodeView.getNextShapeExtent().getMinY();
+				width = childNodeView.getNextShapeExtent().getWidth();
+				height = childNodeView.getNextShapeExtent().getMinY() - y;
+			} else if ((index != 0) && (index < number)) {
+				PLAYNodeView leftChildNodeView = (PLAYNodeView) nodeView
+						.getChild(index - 1);
+				PLAYNodeView rightChildNodeView = (PLAYNodeView) nodeView
+						.getChild(index);
+				x = leftChildNodeView.getNextShapeExtent().getMinX();
+				y = leftChildNodeView.getNextShapeExtent().getMaxY();
+				width = rightChildNodeView.getNextShapeExtent().getWidth();
+				height = rightChildNodeView.getNextShapeExtent().getMinY() - y;
+			} else if (index == number) {
+				PLAYNodeView childNodeView = (PLAYNodeView) nodeView
+						.getChild(number - 1);
+				x = childNodeView.getNextShapeExtent().getMinX();
+				y = childNodeView.getNextShapeExtent().getMaxY();
+				width = childNodeView.getNextShapeExtent().getWidth();
+				height = nodeView.getNextShapeExtent().getMaxY() - y;
+			}
+
+		} else {
+			// no children
+			x = nodeView.getNextShapeExtent().getMinX()
+					+ nodeView.getNextShapeExtent().getWidth() / 10;
+			y = nodeView.getNextShapeExtent().getMinY()
+					+ nodeView.getNextShapeExtent().getHeight() / 10;
+			width = nodeView.getNextShapeExtent().getWidth() * 4 / 5;
+			height = nodeView.getNextShapeExtent().getHeight() * 4 / 5;
+		}
+		zone.setNextShape(new Rectangle2D.Double(x, y, width, height));
+		zone.placeNext(x, y);
+		super.moveZone(zone);
+	}
 
 }
--- trunk/play/src/play/higraph/view/layout/SEQNodeViewLayout.java
+++ trunk/play/src/play/higraph/view/layout/SEQNodeViewLayout.java
@@ -19,54 +19,54 @@ import play.higraph.view.SEQNodeView;
  */
 public class SEQNodeViewLayout extends PLAYLayoutManager {
 
-    /**
-     * @see play.higraph.view.layout.PLAYLayoutManager#layoutLocal(play.higraph.view.PLAYHigraphView)
-     */
-    @Override
-    public void layoutLocal(PLAYHigraphView higraphView) {
-    }
-
-    /**
-     * @see play.higraph.view.layout.PLAYLayoutManager#layoutLocal(play.higraph.view.PLAYNodeView)
-     */
-    @Override
-    public void layoutLocal(PLAYNodeView nodeView) {
-	if (nodeView instanceof SEQNodeView) {
-	    SEQNodeView seqNodeView = (SEQNodeView) nodeView;
-	    // remove all drop zones
-	    seqNodeView.removeAllDropZones();
-	    int number = seqNodeView.getNumChildren();
-	    PLAYViewFactory viewFactory = (PLAYViewFactory) seqNodeView
-		    .getHigraphView().getViewFactory();
-	    Rectangle2D rectangle = new Rectangle2D.Double(0, 0, 20, 20);
-	    if (number == 0) {
-		// the first drop zone
-		viewFactory.makeDropZone(seqNodeView);
-	    } else {
-		double x = 20;
-		double y = 20;
-		// drop zone before the child
-		viewFactory.makeDropZone(seqNodeView);
-		PLAYNodeView childNodeView = (PLAYNodeView) seqNodeView
-			.getChild(0);
-		for (int i = 0; i < number; i++) {
-		    // child
-		    childNodeView = (PLAYNodeView) seqNodeView.getChild(i);
-		    childNodeView.doLayout();
-		    childNodeView.placeNextHierarchy(x, y);
-		    Rectangle2D childNextExtent = childNodeView.getNextExtent();
-		    y += 20 + childNextExtent.getHeight();
-		    Rectangle2D.union(rectangle, childNextExtent, rectangle);
-		    childNodeView.getBranch().setVisibility(false);
-		    // drop zone behind the child
-		    viewFactory.makeDropZone(seqNodeView);
-		}
-	    }
-	    rectangle.add(new Point2D.Double(rectangle.getMaxX() + 10,
-		    rectangle.getMaxY() + 10));
-	    seqNodeView.setNextShape(rectangle);
-	    seqNodeView.translateNextHierarchy(0, 0);
+	/**
+	 * @see play.higraph.view.layout.PLAYLayoutManager#layoutLocal(play.higraph.view.PLAYHigraphView)
+	 */
+	@Override
+	public void layoutLocal(PLAYHigraphView higraphView) {
 	}
-    }
+
+	/**
+	 * @see play.higraph.view.layout.PLAYLayoutManager#layoutLocal(play.higraph.view.PLAYNodeView)
+	 */
+	@Override
+	public void layoutLocal(PLAYNodeView nodeView) {
+		if (nodeView instanceof SEQNodeView) {
+			SEQNodeView seqNodeView = (SEQNodeView) nodeView;
+			// remove all drop zones
+			seqNodeView.removeAllDropZones();
+			int number = seqNodeView.getNumChildren();
+			PLAYViewFactory viewFactory = (PLAYViewFactory) seqNodeView
+					.getHigraphView().getViewFactory();
+			Rectangle2D rectangle = new Rectangle2D.Double(0, 0, 90, 30);
+			if (number == 0) {
+				// the first drop zone
+				viewFactory.makeDropZone(seqNodeView);
+			} else {
+				double x = 20;
+				double y = 20;
+				// drop zone before the child
+				viewFactory.makeDropZone(seqNodeView);
+				PLAYNodeView childNodeView = (PLAYNodeView) seqNodeView
+						.getChild(0);
+				for (int i = 0; i < number; i++) {
+					// child
+					childNodeView = (PLAYNodeView) seqNodeView.getChild(i);
+					childNodeView.doLayout();
+					childNodeView.placeNextHierarchy(x, y);
+					Rectangle2D childNextExtent = childNodeView.getNextExtent();
+					y += 20 + childNextExtent.getHeight();
+					Rectangle2D.union(rectangle, childNextExtent, rectangle);
+					childNodeView.getBranch().setVisibility(false);
+					// drop zone behind the child
+					viewFactory.makeDropZone(seqNodeView);
+				}
+			}
+			rectangle.add(new Point2D.Double(rectangle.getMaxX() + 10,
+					rectangle.getMaxY() + 10));
+			seqNodeView.setNextShape(rectangle);
+			seqNodeView.translateNextHierarchy(0, 0);
+		}
+	}
 
 }
