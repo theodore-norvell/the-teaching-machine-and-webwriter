d7ff339aa49401ea71bdac29af2bed2c7b8964b1
ravneetsandhu.87@gmail.com <ravneetsandhu.87@gmail.com@d93f6c20-76b8-25b4-6ab2-a62924e335af>
2013-12-05 04:04:54 +0000

    New class for execution

---
+++ trunk/trunk/play/src/play/executor/Executor.java
@@ -0,0 +1,38 @@
+package play.executor;
+
+import play.higraph.model.PLAYEdge;
+import play.higraph.model.PLAYEdgeLabel;
+import play.higraph.model.PLAYNode;
+import play.higraph.model.PLAYSubgraph;
+import play.higraph.model.PLAYTag;
+import play.higraph.model.PLAYWholeGraph;
+import play.higraph.swing.PLAYHigraphJComponent;
+import play.higraph.view.PLAYNodeView;
+
+public class Executor implements Runnable{
+	private PLAYSubgraph sg;
+	private PLAYWholeGraph wg;
+	private Environment env;
+	private PLAYNodeView view;
+	private PLAYHigraphJComponent hj;
+	
+	public Executor(PLAYSubgraph sg,PLAYHigraphJComponent hj){
+		this.sg = sg;
+		this.hj = hj;
+		wg = sg.getWholeGraph();
+	}
+	
+	public void run(){
+		System.out.println(sg.getNumberOfTops());
+		env = new Environment(null);
+		String s = null;
+		for(PLAYNode n : sg.getTops()){
+			view  = n.getView();
+			if(n.getTag() == PLAYTag.CLASS){
+				System.out.println("class");
+				s = view.execute(env,n,sg);				
+			}
+		}
+		System.out.println(s);
+	}
+}
