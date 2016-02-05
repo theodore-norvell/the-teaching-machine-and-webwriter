5ddab060afe0e99a0c3eed0db61b80a91f049b79
Theodore Norvell <theo@mun.ca>
2016-02-05 17:40:00 -0330

    Updating Fill in the Expression Code.

--- trunk/JSTM/src/go.ts
+++ trunk/JSTM/src/go.ts
@@ -2,43 +2,6 @@
 /// <reference path="MockJSTM.ts" />
 /// <reference path="addTM.ts" />
 
-class FITEController {
-    enum FITEControllerState {
-        init = 1, started = 2 }
-
-    var _tm : jstm.JSTM ;
-    var _startButton : HTMLElement ;
-    var _expression : HTMLElement ;
-    var _valueBoxes = new Array<HTMLElement>() ;
-    var _code : string
-
-    var state : FITEControllerState = FITEControllerState.init ;
-
-    constructor( tm : jstm.JSTM,
-                 startButton : HTMLElement,
-                 expression : HTMLElement,
-                 valueBoxes : Array<HTMLElement>,
-                 code : string ) {
-        this._tm = tm ;
-        this._startButton = startButton ;
-        this._expression = expression ;
-        valueBoxes.forEach( (el) => this._valueBoxes.push(el) ) ;
-        this._code = code ;
-
-        // Add event handlers
-        this._startButton.onclick = () => this.start() ;
-        this._valueBoxes.forEach( (el) =>
-                el.onchange = (() => this.change() ) ) ;
-        this._expression.onchange = () => this.change() ;
-    }
-
-    start() : void {
-        if( state ==  FITEControllerState.init ) {
-            this._tm.loadString( newCode, this._language ) ;
-            var status = 
-
-}
-
 function go() : void {
     var tm : jstm.JSTM = new jstm.MockJSTM() ;
     var root : HTMLElement = document.documentElement ;
