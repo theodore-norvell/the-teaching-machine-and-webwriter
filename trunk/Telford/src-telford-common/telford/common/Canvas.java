b0184ffd9f9517961eeff50804ed1d7365e78a56
helengrape <xl5208@mun.ca>
2016-05-24 13:03:15 -0230

    changes for gwt
    add a new class telford.common.Canvas.java
    add a new class telford.common.CanvasPeer.java
    change class telford.common.Kit.java

--- trunk/Telford/src-telford-common/telford/common/Canvas.java
+++ trunk/Telford/src-telford-common/telford/common/Canvas.java
@@ -1,5 +1,25 @@
 package telford.common;
 
-public class Canvas extends Component {
+import telford.common.peers.CanvasPeer;
 
+public class Canvas extends Component {
+	
+	public Canvas() { this(true) ; }
+	
+	public Canvas (boolean makePeer )  {
+		super(false) ;
+		if( makePeer ) {
+			peer = Kit.getKit().makeCanvasPeer(this);
+		}
+	}
+		
+	@Override
+	public CanvasPeer getPeer() {
+		return (CanvasPeer) peer;
+	}
+
+	@Override
+	public void paintComponent(Graphics g){
+	}
+	
 }
---
+++ trunk/Telford/src-telford-common/telford/common/peers/CanvasPeer.java
@@ -0,0 +1,10 @@
+package telford.common.peers;
+
+import telford.common.Canvas;
+
+public abstract class CanvasPeer extends ComponentPeer {
+	
+	public CanvasPeer( Canvas canvas) {
+		super(canvas);
+	}
+}
